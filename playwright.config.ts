4985939f7bac29b09a4052610b98fe4d3daac741
Julien Pequegnot <julienmika@Juliens-iMac-2.local>
2026-08-25 04:36:23 -0400

    fix: run mobile browser checks on Chromium

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
   },
   projects: [
     { name: "chromium", use: { ...devices["Desktop Chrome"] } },
-    { name: "mobile", use: { ...devices["iPhone 13"] } }
+    { name: "mobile", use: { ...devices["Pixel 7"], browserName: "chromium" } }
   ]
 });
